2f4305b9562d118317a60fa50ace5785012c66d5
lijialiang <lijialiang@chexiang.com>
2018-02-07 19:09:57 +0800

    commit init

---
+++ src/main/java/com/jeffery/lijl/web/controller/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C), 2013-2018, 上海汽车集团股份有限公司
+ * FileName: package-info.java
+ * Author:   lijialiang
+ * Date:     2018年2月7日 下午7:07:17
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈功能详细描述〉
+ *
+ * @author lijialiang
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+package com.jeffery.lijl.web.controller;
